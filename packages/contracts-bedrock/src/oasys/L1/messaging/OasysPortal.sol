de6b3a85684ab093f1ec1b68ff93e7eb5de804f6
ironbeer <7997273+ironbeer@users.noreply.github.com>
2024-02-19 16:54:07 +0900

    - Removed the batch proof method. - Instead of overriding the Finalize method, it now checks for a message relayer within _isFinalizationPeriodElapsed.

--- packages/contracts-bedrock/src/oasys/L1/messaging/OasysPortal.sol
+++ packages/contracts-bedrock/src/oasys/L1/messaging/OasysPortal.sol
@@ -45,113 +45,9 @@ contract OasysPortal is OptimismPortal {
         emit MessageRelayerSet(newRelayer);
     }
 
-    /// @notice Finalizes a withdrawal transaction.
-    function finalizeWithdrawalTransaction(Types.WithdrawalTransaction calldata _tx) public override whenNotPaused {
-        // Make sure that the l2Sender has not yet been set. The l2Sender is set to a value other
-        // than the default value when a withdrawal transaction is being finalized. This check is
-        // a defacto reentrancy guard.
-        require(l2Sender == Constants.DEFAULT_L2_SENDER, "OasysPortal: can only trigger one withdrawal per transaction");
-
-        // Grab the proven withdrawal from the `provenWithdrawals` map.
-        bytes32 withdrawalHash = Hashing.hashWithdrawal(_tx);
-        ProvenWithdrawal memory provenWithdrawal = provenWithdrawals[withdrawalHash];
-
-        // A withdrawal can only be finalized if it has been proven. We know that a withdrawal has
-        // been proven at least once when its timestamp is non-zero. Unproven withdrawals will have
-        // a timestamp of zero.
-        require(provenWithdrawal.timestamp != 0, "OasysPortal: withdrawal has not been proven yet");
-
-        // As a sanity check, we make sure that the proven withdrawal's timestamp is greater than
-        // starting timestamp inside the L2OutputOracle. Not strictly necessary but extra layer of
-        // safety against weird bugs in the proving step.
-        require(
-            provenWithdrawal.timestamp >= L2_ORACLE.startingTimestamp(),
-            "OasysPortal: withdrawal timestamp less than L2 Oracle starting timestamp"
-        );
-
-        // If the caller is not an message relayer,
-        // a proven withdrawal must wait at least the finalization period before it can be
-        // finalized. This waiting period can elapse in parallel with the waiting period for the
-        // output the withdrawal was proven against. In effect, this means that the minimum
-        // withdrawal time is proposal submission time + finalization period.
-        if (msg.sender != messageRelayer) {
-            require(
-                super._isFinalizationPeriodElapsed(provenWithdrawal.timestamp),
-                "OasysPortal: proven withdrawal finalization period has not elapsed"
-            );
-        }
-
-        // Grab the OutputProposal from the L2OutputOracle, will revert if the output that
-        // corresponds to the given index has not been proposed yet.
-        Types.OutputProposal memory proposal = L2_ORACLE.getL2Output(provenWithdrawal.l2OutputIndex);
-
-        // Check that the output root that was used to prove the withdrawal is the same as the
-        // current output root for the given output index. An output root may change if it is
-        // deleted by the challenger address and then re-proposed.
-        require(
-            proposal.outputRoot == provenWithdrawal.outputRoot,
-            "OasysPortal: output root proven is not the same as current output root"
-        );
-
-        // Check that the output proposal has also been finalized.
-        require(
-            _isFinalizationPeriodElapsed(proposal.timestamp),
-            "OasysPortal: output proposal finalization period has not elapsed"
-        );
-
-        // Check that this withdrawal has not already been finalized, this is replay protection.
-        require(finalizedWithdrawals[withdrawalHash] == false, "OasysPortal: withdrawal has already been finalized");
-
-        // Mark the withdrawal as finalized so it can't be replayed.
-        finalizedWithdrawals[withdrawalHash] = true;
-
-        // Set the l2Sender so contracts know who triggered this withdrawal on L2.
-        l2Sender = _tx.sender;
-
-        // Trigger the call to the target contract. We use a custom low level method
-        // SafeCall.callWithMinGas to ensure two key properties
-        //   1. Target contracts cannot force this call to run out of gas by returning a very large
-        //      amount of data (and this is OK because we don't care about the returndata here).
-        //   2. The amount of gas provided to the execution context of the target is at least the
-        //      gas limit specified by the user. If there is not enough gas in the current context
-        //      to accomplish this, `callWithMinGas` will revert.
-        bool success = SafeCall.callWithMinGas(_tx.target, _tx.gasLimit, _tx.value, _tx.data);
-
-        // Reset the l2Sender back to the default value.
-        l2Sender = Constants.DEFAULT_L2_SENDER;
-
-        // All withdrawals are immediately finalized. Replayability can
-        // be achieved through contracts built on top of this contract
-        emit WithdrawalFinalized(withdrawalHash, success);
-
-        // Reverting here is useful for determining the exact gas cost to successfully execute the
-        // sub call to the target contract if the minimum gas limit specified by the user would not
-        // be sufficient to execute the sub call.
-        if (success == false && tx.origin == Constants.ESTIMATION_ADDRESS) {
-            revert("OasysPortal: withdrawal failed");
-        }
-    }
-
-    /// @notice Batch proves withdrawal transaction.
-    function proveWithdrawalTransactions(
-        Types.WithdrawalTransaction[] calldata _txs,
-        uint256[] calldata _l2OutputIndexes,
-        Types.OutputRootProof[] calldata _outputRootProofs,
-        bytes[][] calldata _withdrawalProofs
-    )
-        external
-    {
-        uint256 length = _txs.length;
-        require(length == _l2OutputIndexes.length, "OasysPortal: array lengths must be equal");
-        require(length == _outputRootProofs.length, "OasysPortal: array lengths must be equal");
-        require(length == _withdrawalProofs.length, "OasysPortal: array lengths must be equal");
-
-        for (uint256 i = 0; i < length; i++) {
-            proveWithdrawalTransaction(_txs[i], _l2OutputIndexes[i], _outputRootProofs[i], _withdrawalProofs[i]);
-        }
-    }
-
     /// @notice Batch finalizes withdrawal transactions.
+    ///         This method is necessary because it is not possible to check
+    ///         whether msg.sender is a message relayer via the Multicall contract.
     function finalizeWithdrawalTransactions(Types.WithdrawalTransaction[] calldata _txs) external {
         uint256 length = _txs.length;
         for (uint256 i = 0; i < length; i++) {
@@ -161,12 +57,18 @@ contract OasysPortal is OptimismPortal {
 
     /// @notice Determines whether the finalization period has elapsed with respect to
     ///         the provided block timestamp.
+    ///         If the caller is an authorized message relayer and the timestamp
+    ///         is immediately verified, no need to wait for the finalization period.
     /// @param _timestamp Timestamp to check.
     /// @return Whether or not the finalization period has elapsed.
     function _isFinalizationPeriodElapsed(uint256 _timestamp) internal view override returns (bool) {
-        uint256 verified = IOasysL2OutputOracle(address(L2_ORACLE)).verifiedL1Timestamp();
-        if (verified > 0 && verified > _timestamp) {
-            return true;
+        if (messageRelayer != address(0) && msg.sender == messageRelayer) {
+            // Note: If called with `eth_call`, this code block
+            //       will not pass because `msg.sender` is fixed at zero address.
+            uint256 verified = IOasysL2OutputOracle(address(L2_ORACLE)).verifiedL1Timestamp();
+            if (verified > _timestamp) {
+                return true;
+            }
         }
         return super._isFinalizationPeriodElapsed(_timestamp);
     }
--- packages/contracts-bedrock/src/oasys/L1/rollup/OasysRollupVerifier.sol
+++ packages/contracts-bedrock/src/oasys/L1/rollup/OasysRollupVerifier.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.15;
 
 import { Types } from "src/libraries/Types.sol";
 import { OasysStateCommitmentChainVerifier } from "src/oasys/L1/build/legacy/OasysStateCommitmentChainVerifier.sol";
-import { OasysL2OutputOracle } from "src/oasys/L1/rollup/OasysL2OutputOracle.sol";
+import { IOasysL2OutputOracle } from "src/oasys/L1/interfaces/IOasysL2OutputOracle.sol";
 
 /// @title OasysRollupVerifier
 contract OasysRollupVerifier is OasysStateCommitmentChainVerifier {
@@ -12,13 +12,11 @@ contract OasysRollupVerifier is OasysStateCommitmentChainVerifier {
     event L2OutputRejected(address indexed l2OutputOracle, uint256 indexed l2OutputIndex, bytes32 indexed outputRoot);
 
     /// @notice Approve the L2 output.
-    /// @param senderSignature Signature of the original message sender.
-    /// @param l2OutputOracle  Address of the target L2OutputOracle.
-    /// @param l2OutputIndex   Index of the target L2Output.
-    /// @param l2Output        Target L2 Output.
-    /// @param signatures      List of signatures.
+    /// @param l2OutputOracle Address of the target L2OutputOracle.
+    /// @param l2OutputIndex  Index of the target L2Output.
+    /// @param l2Output       Target L2 Output.
+    /// @param signatures     List of signatures.
     function approve(
-        bytes calldata senderSignature,
         address l2OutputOracle,
         uint256 l2OutputIndex,
         Types.OutputProposal calldata l2Output,
@@ -26,23 +24,19 @@ contract OasysRollupVerifier is OasysStateCommitmentChainVerifier {
     )
         external
     {
-        bytes32 msgHash = _getMsgHash(l2OutputOracle, l2OutputIndex, l2Output, true);
-        _verifySignatures(msgHash, signatures);
+        _verifySignatures(_getMsgHash(l2OutputOracle, l2OutputIndex, l2Output, true), signatures);
 
-        address originalSender = _recoverSigner(msgHash, senderSignature);
-        OasysL2OutputOracle(l2OutputOracle).succeedVerification(originalSender, l2OutputIndex, l2Output);
+        IOasysL2OutputOracle(l2OutputOracle).succeedVerification(l2OutputIndex, l2Output);
 
         emit L2OutputApproved(l2OutputOracle, l2OutputIndex, l2Output.outputRoot);
     }
 
     /// @notice Reject the L2 output.
-    /// @param senderSignature Signature of the original message sender.
-    /// @param l2OutputOracle  Address of the target L2OutputOracle.
-    /// @param l2OutputIndex   Index of the target L2Output.
-    /// @param l2Output        Target L2 Output.
-    /// @param signatures      List of signatures.
+    /// @param l2OutputOracle Address of the target L2OutputOracle.
+    /// @param l2OutputIndex  Index of the target L2Output.
+    /// @param l2Output       Target L2 Output.
+    /// @param signatures     List of signatures.
     function reject(
-        bytes calldata senderSignature,
         address l2OutputOracle,
         uint256 l2OutputIndex,
         Types.OutputProposal calldata l2Output,
@@ -50,11 +44,9 @@ contract OasysRollupVerifier is OasysStateCommitmentChainVerifier {
     )
         external
     {
-        bytes32 msgHash = _getMsgHash(l2OutputOracle, l2OutputIndex, l2Output, false);
-        _verifySignatures(msgHash, signatures);
+        _verifySignatures(_getMsgHash(l2OutputOracle, l2OutputIndex, l2Output, false), signatures);
 
-        address originalSender = _recoverSigner(msgHash, senderSignature);
-        OasysL2OutputOracle(l2OutputOracle).failVerification(originalSender, l2OutputIndex, l2Output);
+        IOasysL2OutputOracle(l2OutputOracle).failVerification(l2OutputIndex, l2Output);
 
         emit L2OutputRejected(l2OutputOracle, l2OutputIndex, l2Output.outputRoot);
     }
@@ -78,8 +70,11 @@ contract OasysRollupVerifier is OasysStateCommitmentChainVerifier {
             block.chainid,
             l2OutputOracle,
             l2OutputIndex,
-            // Prevent reuse of signature by including L1 timestamp.
-            abi.encodePacked(l2Output.outputRoot, l2Output.timestamp, l2Output.l2BlockNumber),
+            abi.encodePacked(
+                l2Output.outputRoot,
+                l2Output.timestamp, // Prevent reuse of signature by including L1 timestamp.
+                l2Output.l2BlockNumber
+            ),
             approved
         );
         return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(signData)));
