de361c31fb4ae1b19e8401b082c9b74288ef43cf
tak <re795h@gmail.com>
2026-02-02 18:48:49 +0900

    fix copilot waring

--- packages/contracts-bedrock/src/oasys/L1/close/ClosedL1ERC721Bridge.sol
+++ packages/contracts-bedrock/src/oasys/L1/close/ClosedL1ERC721Bridge.sol
@@ -3,19 +3,14 @@ pragma solidity 0.8.15;
 
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import { OasysL1ERC721Bridge } from "src/oasys/L1/messaging/OasysL1ERC721Bridge.sol";
-import { SystemConfig } from "src/L1/SystemConfig.sol";
 import { IL1BuildAgent } from "src/oasys/L1/build/interfaces/IL1BuildAgent.sol";
+import { SystemConfigOwnerResolver } from "src/oasys/L1/close/SystemConfigOwnerResolver.sol";
 
 /// @title ClosedL1ERC721Bridge
 /// @notice L1 ERC721 bridge in a "closed" state.
-contract ClosedL1ERC721Bridge is OasysL1ERC721Bridge {
-    /// @notice Reference to the L1 build agent used to resolve system config and built addresses per chain.
-    IL1BuildAgent public immutable L1_BUILD_AGENT;
-
+contract ClosedL1ERC721Bridge is OasysL1ERC721Bridge, SystemConfigOwnerResolver {
     /// @param _l1buildAgent L1 build agent for chain config and built address lookup.
-    constructor(IL1BuildAgent _l1buildAgent) {
-        L1_BUILD_AGENT = _l1buildAgent;
-    }
+    constructor(IL1BuildAgent _l1buildAgent) SystemConfigOwnerResolver(_l1buildAgent) { }
 
     /// @notice Withdraws ERC721 tokens held by this bridge to a specified address.
     /// @param _chainId Chain ID used to resolve system config and final system owner.
@@ -33,11 +28,10 @@ contract ClosedL1ERC721Bridge is OasysL1ERC721Bridge {
     {
         address finalSystemOwner = _getOwnerFromSystemConfig(_chainId);
         require(finalSystemOwner == msg.sender, "not final system owner");
-        (,,,,,, address l1ERC721BridgeProxy,,) = L1_BUILD_AGENT.builtLists(_chainId);
+        (,,, address l1ERC721BridgeProxy,,,,,) = L1_BUILD_AGENT.builtLists(_chainId);
         require(l1ERC721BridgeProxy == address(this), "not the bridge");
 
         for (uint256 i = 0; i < _tokenIds.length; ++i) {
-            require(IERC721(_erc721).ownerOf(_tokenIds[i]) == address(this), "not owned by bridge");
             IERC721(_erc721).safeTransferFrom(address(this), _to, _tokenIds[i]);
         }
     }
@@ -57,18 +51,5 @@ contract ClosedL1ERC721Bridge is OasysL1ERC721Bridge {
         override
     {
         revert("bridge is closed");
-    }
-
-    /// @notice Returns the final system owner and built address list for a chain.
-    /// @param _chainId Chain ID to look up.
-    /// @return Final system owner address
-    function _getOwnerFromSystemConfig(uint256 _chainId) internal view returns (address) {
-        // Validate the chain id
-        (,,, address systemConfigPorxy,,,,,) = L1_BUILD_AGENT.builtLists(_chainId);
-        require(systemConfigPorxy != address(0), "invalid chain id");
-
-        // Get the owner as well as the build list
-        SystemConfig systemConfig = SystemConfig(systemConfigPorxy);
-        return systemConfig.owner();
     }
 }
--- packages/contracts-bedrock/src/oasys/L1/close/L1CloseAgent.sol
+++ packages/contracts-bedrock/src/oasys/L1/close/L1CloseAgent.sol
@@ -5,9 +5,9 @@ import { ISemver } from "src/universal/ISemver.sol";
 import { ProxyAdmin } from "src/universal/ProxyAdmin.sol";
 import { Proxy } from "src/universal/Proxy.sol";
 import { L1ChugSplashProxy } from "src/legacy/L1ChugSplashProxy.sol";
-import { SystemConfig } from "src/L1/SystemConfig.sol";
 import { IL1BuildAgent } from "src/oasys/L1/build/interfaces/IL1BuildAgent.sol";
 import { ILegacyL1BuildAgent } from "src/oasys/L1/build/interfaces/ILegacyL1BuildAgent.sol";
+import { SystemConfigOwnerResolver } from "src/oasys/L1/close/SystemConfigOwnerResolver.sol";
 
 interface ILegacyOptimismPortal {
     function GUARDIAN() external view returns (address);
@@ -17,13 +17,7 @@ interface ILegacyOptimismPortal {
 
 /// @title L1CloseAgent
 /// @notice This contract handles the closing/shutdown of Verse chains that were previously built by L1BuildAgent.
-contract L1CloseAgent is ISemver {
-    /// @notice Reference to the legacy L1BuildAgent (V1)
-    ILegacyL1BuildAgent public immutable LEGACY_L1_BUILD_AGENT;
-
-    /// @notice Reference to the current L1BuildAgent (V2)
-    IL1BuildAgent public immutable L1_BUILD_AGENT;
-
+contract L1CloseAgent is ISemver, SystemConfigOwnerResolver {
     /// @notice Implementation address for the closed L1StandardBridge
     address public immutable CLOSED_L1_STANDARD_BRIDGE;
 
@@ -38,20 +32,18 @@ contract L1CloseAgent is ISemver {
     string public constant version = "1.0.0";
 
     /// @notice Constructor sets references to build agents and closed bridge implementations
-    /// @param _legacyL1BuildAgent Reference to legacy build agent (same as L1BuildAgent.LEGACY_L1_BUILD_AGENT)
     /// @param _l1buildAgent Reference to current build agent that created the chains this contract will close
     /// @param _closedL1StandardBridge Implementation address for the closed L1StandardBridge
     /// @param _closedL1ERC721Bridge Implementation address for the closed L1ERC721Bridge
     /// @param _closedL1CrossDomainMessenger Implementation address for the closed L1CrossDomainMessenger
     constructor(
-        ILegacyL1BuildAgent _legacyL1BuildAgent,
         IL1BuildAgent _l1buildAgent,
         address _closedL1StandardBridge,
         address _closedL1ERC721Bridge,
         address _closedL1CrossDomainMessenger
-    ) {
-        LEGACY_L1_BUILD_AGENT = _legacyL1BuildAgent;
-        L1_BUILD_AGENT = _l1buildAgent;
+    )
+        SystemConfigOwnerResolver(_l1buildAgent)
+    {
         CLOSED_L1_STANDARD_BRIDGE = _closedL1StandardBridge;
         CLOSED_L1_ERC721_BRIDGE = _closedL1ERC721Bridge;
         CLOSED_L1_CROSS_DOMAIN_MESSENGER = _closedL1CrossDomainMessenger;
@@ -65,18 +57,18 @@ contract L1CloseAgent is ISemver {
         require(finalSystemOwner == msg.sender, "not final system owner");
         // Validate the chain id
         (
-            address proxyAdminAddr,
-            address oasysPortalProxy,
+            address _proxyAdmin,
             ,
-            ,
-            address l1CrossDomainMessengerProxy,
             address l1StandardBridgeProxy,
             address l1ERC721BridgeProxy,
+            address l1CrossDomainMessengerProxy,
+            ,
+            address oasysPortalProxy,
             ,
         ) = L1_BUILD_AGENT.builtLists(_chainId);
 
         // Make sure this contract is the proxy admin owner
-        ProxyAdmin proxyAdmin = ProxyAdmin(proxyAdminAddr);
+        ProxyAdmin proxyAdmin = ProxyAdmin(_proxyAdmin);
         require(proxyAdmin.owner() == address(this), "not transferred admin");
 
         // Upgrade Bridges to closed implementations
@@ -106,10 +98,10 @@ contract L1CloseAgent is ISemver {
         // Only the final system owner can transfer proxy admin ownership
         address finalSystemOwner = getOwnerFromSystemConfig(_chainId);
         require(finalSystemOwner == msg.sender, "not final system owner");
-        (address proxyAdminAddr,,,,,,,,) = L1_BUILD_AGENT.builtLists(_chainId);
+        (address _proxyAdmin,,,,,,,,) = L1_BUILD_AGENT.builtLists(_chainId);
 
         // Make sure this contract is the proxy admin owner
-        ProxyAdmin proxyAdmin = ProxyAdmin(proxyAdminAddr);
+        ProxyAdmin proxyAdmin = ProxyAdmin(_proxyAdmin);
         require(proxyAdmin.owner() == address(this), "not transferred admin");
 
         // Transfer back the ownership to the final system owner
@@ -120,21 +112,6 @@ contract L1CloseAgent is ISemver {
     /// @param _chainId The chainId of the Verse
     /// @return The final system owner address
     function getOwnerFromSystemConfig(uint256 _chainId) public view returns (address) {
-        // Validate the chain id
-        (,,, address systemConfigPorxy,,,,,) = L1_BUILD_AGENT.builtLists(_chainId);
-        require(systemConfigPorxy != address(0), "invalid chain id");
-
-        // Get the owner as well as the build list
-        SystemConfig systemConfig = SystemConfig(systemConfigPorxy);
-        return systemConfig.owner();
-    }
-
-    /// @notice Check if the L2 is a legacy L2 built by the V1 build agent
-    /// @param _chainId The chainId of Verse
-    /// @return bool indicating if this is a legacy L2, and the AddressManager address if it exists
-    function _isLegacyL2(uint256 _chainId) internal view returns (bool, address) {
-        // If AddressManager exists, it's legacy
-        address addressManager = LEGACY_L1_BUILD_AGENT.getAddressManager(_chainId);
-        return (addressManager != address(0), addressManager);
+        return _getOwnerFromSystemConfig(_chainId);
     }
 }
